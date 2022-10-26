17e752bd5a1601f3f2db4394a481d321874b2e6a
soloist <w1053904672@163.com>
2022-10-26 18:18:48 +0800

    update

--- yolov5-6.2/models/custom/GSConv_SlimNeck.py
+++ yolov5-6.2/models/custom/GSConv_SlimNeck.py
@@ -18,13 +18,11 @@ class GSConv(nn.Module):
         # y = x2.reshape(x2.shape[0], 2, x2.shape[1] // 2, x2.shape[2], x2.shape[3])
         # y = y.permute(0, 2, 1, 3, 4)
         # return y.reshape(y.shape[0], -1, y.shape[3], y.shape[4])
-
         b, n, h, w = x2.data.size()
         b_n = b * n // 2
         y = x2.reshape(b_n, 2, h * w)
         y = y.permute(1, 0, 2)
         y = y.reshape(2, -1, n // 2, h, w)
-
         return torch.cat((y[0], y[1]), 1)
 
 
